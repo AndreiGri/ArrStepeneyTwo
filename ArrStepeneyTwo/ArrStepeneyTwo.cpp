db8a6f5b9d7064846250aea9323ec71bd822741e
Gridin_Andrei <andreigridin918@gmail.com>
2023-08-11 12:04:36 +0500

    Добавьте файлы проекта.

---
+++ ArrStepeneyTwo/ArrStepeneyTwo.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251 > nul");
+    int n, y = 0;
+    const int i = 5;
+    int arr[i];
+    cout << "Введите с какой степени начать заполнение массива: ";
+    cin >> n;
+    while (y < i) {
+        arr[y] = pow(2, n);
+        cout << arr[y] << " ";
+        n++;
+        y++;
+    }
+    system("pause > nul");
+    return 0;
+}
+
